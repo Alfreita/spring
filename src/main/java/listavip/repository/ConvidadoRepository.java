5242da5027d0503bf6c05fc22bcb2f86d7d531c5
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-08-14 15:39:47 -0300

    adição do comentario em ConvidadoRepository e da dependencia de Spring DevTools

--- src/main/java/listavip/repository/ConvidadoRepository.java
+++ src/main/java/listavip/repository/ConvidadoRepository.java
@@ -3,5 +3,8 @@ package listavip.repository;
 import listavip.modelo.Convidado;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * CrudReposytory serve para ter todas as implementações prontas logo de cara
+ */
 public interface ConvidadoRepository extends CrudRepository<Convidado,Long> {
 }
